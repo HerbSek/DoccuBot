2adcf580ff9223d977d5593b3fcafa9d58c55e1e
Herbert Sekpey <69324217+HerbSek@users.noreply.github.com>
2024-09-08 15:40:34 +0000

    Update docubot.py

--- docubot.py
+++ docubot.py
@@ -13,6 +13,16 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+hide_streamlit_watermark = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    .stApp { bottom: 0px; }
+    .viewerBadge_container__1QSob { display: none !important; }
+    </style>
+    """
+st.markdown(hide_streamlit_watermark, unsafe_allow_html=True)
+
 def response_generator(prompt):
     response = "Lets start working!!"
     for word in response.split():
