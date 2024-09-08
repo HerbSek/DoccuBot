dbc1c0eb5afa07b2d46efdd917325aec9df84aa8
Herbert Sekpey <69324217+HerbSek@users.noreply.github.com>
2024-09-08 15:26:48 +0000

    Update docubot.py

--- docubot.py
+++ docubot.py
@@ -15,6 +15,10 @@ col1,col2,col3 = st.columns(3)
 
 with col1:
     st.write('An interactive tool used to chat with your pdf documents')
+with col2:
+    st.write('An interactive tool used to chat with your pdf documents')
+with col3:
+    st.write('An interactive tool used to chat with your pdf documents')
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
